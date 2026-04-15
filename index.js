ddec54bcd8754cdb4889fb998d767bc9cedd1bd5
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-04-15 19:24:08 +0000

    first?

--- index.js
+++ index.js
@@ -23,3 +23,32 @@ class Car{
         this.year = year;
     }
 }
+
+class Agable{
+    constructor(year, age){
+        this.year = year;
+        this.age = age;
+    }
+
+    draw() {
+        return `<select id="${this.year}" name= "${this.name}">
+        ${use.new.Date().getFullYear().to.get.the.current.year}
+        </select>`
+    }
+
+
+
+}
+
+//should create a class named Agable that takes a year and has an age 
+// method that calculates the age (current year - objects year) 
+// (use new Date().getFullYear() to get the current year)
+
+//should inherit from Agable in Company class and pass yearEstablished 
+// to Agable in the constructor
+
+//should inherit from Agable in Person class and pass birthYear to 
+// Agable in the constructor
+
+//should inherit from Agable in Car class and pass year to Agable in 
+// the constructor
