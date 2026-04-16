ee3599b3878b293d9c5df995c2d2b49405bc664f
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-04-16 18:58:00 +0000

    sdcbhj

--- index.js
+++ index.js
@@ -1,43 +1,75 @@
-class Company{
-    constructor(name, taxId, yearEstablished, taxRate){
+//class Company{
+//    constructor(name, taxId, yearEstablished, taxRate){
+      //  this.name = name;
+    //    this.id = taxId;
+  //      this.year = yearEstablished;
+//        this.taxRate = taxRate;
+   // }
+//}
+
+//class Person{
+    //constructor(name, ssn, birthYear, taxRate){
+  //      this.name = name;
+//        //this.id = ssn;
+      //  this.year = birthYear;
+    //    this.taxRate = taxRate;
+  //  }
+//}
+
+//class Car{
+//    constructor(model, vin, year){
+    //    this.name = model;
+  //      this.id = vin;
+//        this.year = year;
+//    }
+//}
+
+//class Agable{
+   // constructor(year){
+ //       this.year = year;
+   // }
+
+ //   draw() {
+//        return `<select id="${this.year}" age= "${this.age}">
+  //      ${use.new.Date().getFullYear().to.get.the.current.year}
+//    </select>`
+  //  }
+//}
+
+class Agable {
+    constructor(year) {
+        this.year = year;
+    }
+
+    age() {
+        return new Date().getFullYear() - this.year;
+    }
+}
+
+class Company extends Agable {
+    constructor(name, taxId, yearEstablished, taxRate) {
+        super(yearEstablished);   // pass year to Agable
         this.name = name;
         this.id = taxId;
-        this.year = yearEstablished;
         this.taxRate = taxRate;
     }
 }
 
-class Person{
-    constructor(name, ssn, birthYear, taxRate){
+class Person extends Agable {
+    constructor(name, ssn, birthYear, taxRate) {
+        super(birthYear);         // pass birthYear to Agable
         this.name = name;
         this.id = ssn;
-        this.year = birthYear;
         this.taxRate = taxRate;
     }
 }
 
-class Car{
-    constructor(model, vin, year){
+class Car extends Agable {
+    constructor(model, vin, year) {
+        super(year);              // pass car year to Agable
         this.name = model;
         this.id = vin;
-        this.year = year;
     }
-}
-
-class Agable{
-    constructor(year, age){
-        this.year = year;
-        this.age = age;
-    }
-
-    draw() {
-        return `<select id="${this.year}" age= "${this.age}">
-        ${use.new.Date().getFullYear().to.get.the.current.year}
-        </select>`
-    }
-
-
-
 }
 
 //should create a class named Agable that takes a year and has an age 
